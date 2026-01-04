26fec15d186b2c4b81a2c92292b6952772c3feeb
IndiaCabo <india.cabo@edu.esiee.fr>
2026-01-04 16:14:55 +0100

    Avancement front

---
+++ src/pages/index.py
@@ -0,0 +1,78 @@
+from dash import Dash, dcc, html
+
+app = Dash()
+
+app.layout = html.Div([
+    html.H1("Dashboard - World Air Quality", style={'text-align':'center'}, className="page-title"),
+    html.Div([
+        dcc.Slider(
+            min=2015, 
+            max=2025, 
+            step=1,
+            marks={i: str(i) for i in range(2015, 2026)},
+            id="year-slider",
+            className="custom-slider"
+        ),
+    ], style={'display': 'flex', 'justify-content': 'center', 'margin':'3rem'}),
+    
+    html.Div([
+        html.Div([
+            html.H6("Nombre de pays comptés", className='valeur-mesuree'),
+            html.H3(id="nb-pays")
+        ]),
+        html.Div([
+            html.H6("Polluant mesuré", className='valeur-mesuree'),
+            html.H3(id="polluant")
+        ]),
+    ], style={'display': 'flex', 'justify-content': 'space-evenly', 'margin':'3rem'}),
+    
+    html.H1("CARTE", style={'text-align':'center'}, className="page-title"),
+    
+    html.Div([
+        html.Div([
+            html.Span([
+                "PM2.5 : Particules fines (µg/m³)",
+                html.Span(
+                    "Les particules fines (PM2,5) sont des entités solides de très petite taille, nocives pour la santé respiratoire et cardiovasculaire",
+                    className='tooltiptext'
+                )
+            ], className='tooltip'),
+            html.Br(),
+            html.Span([
+                "PM10 : Particules grossières (µg/m³)",
+                html.Span(
+                    "Les particules fines (PM2,5) sont des entités solides de très petite taille, nocives pour la santé respiratoire et cardiovasculaire",
+                    className='tooltiptext'
+                )
+            ], className='tooltip'),
+            html.Br(),
+            html.Span([
+                "NO2 : Dioxyde d'azote (µg/m³)",
+                html.Span(
+                    "Les particules fines (PM2,5) sont des entités solides de très petite taille, nocives pour la santé respiratoire et cardiovasculaire",
+                    className='tooltiptext'
+                )
+            ], className='tooltip'),
+            html.Br(),
+            html.Span([
+                "O3 : Ozone (µg/m³)",
+                html.Span(
+                    "Les particules fines (PM2,5) sont des entités solides de très petite taille, nocives pour la santé respiratoire et cardiovasculaire",
+                    className='tooltiptext'
+                )
+            ], className='tooltip'),
+            html.Br(),
+            html.Span([
+                "CO2 : Émissions de dioxyde de carbone (tonnes)",
+                html.Span(
+                    "Les particules fines (PM2,5) sont des entités solides de très petite taille, nocives pour la santé respiratoire et cardiovasculaire",
+                    className='tooltiptext'
+                )
+            ], className='tooltip'),
+        ], className='legende'),
+    ], style={'display': 'flex', 'justify-content':'center'}),
+    
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
