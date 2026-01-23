85313a338327e2cf9bb6aca4a562d1ab33539a1a
IndiaCabo <india.cabo@edu.esiee.fr>
2026-01-23 10:48:13 +0100

    click sur graphique = hide map et show graph

--- src/components/navbar.py
+++ src/components/navbar.py
@@ -3,13 +3,13 @@ from dash import html
 def create_navbar():
     return html.Nav([
         html.Div([
-            html.A([
+            html.Div([
                 html.Img(src="/assets/img/globe_icon.svg", alt="Globe"),
                 html.Span("Carte")
-            ], href="#carte", className="nav-item"),
-            html.A([
+            ], id="nav-carte", className="nav-item", n_clicks=0),
+            html.Div([
                 html.Img(src="/assets/img/bar_char_icon.svg", alt="Graphique"),
                 html.Span("Graphiques")
-            ], href="#graphiques", className="nav-item"),
+            ], id="nav-graphiques", className="nav-item", n_clicks=0),
         ], className="nav-container")
     ], className="navbar")
